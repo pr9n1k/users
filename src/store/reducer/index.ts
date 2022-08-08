6ed0aea1fa9e0c69fe351c80b579f82711ad9194
pr9n1k <74241094+pr9n1k@users.noreply.github.com>
2022-08-08 17:20:41 +0300

    removing unnecessary

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -3,7 +3,6 @@ import { userReducer } from "./user";
 
 export const rootReducer = combineReducers({
   user: userReducer,
-  // todo: todoReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
